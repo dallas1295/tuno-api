afcbad0d7a7c2f29dc6017b2d04489556f9043d4
Dallas. S <andrewdallassherman@gmail.com>
2025-02-25 14:41:26 +0900

    adding response interfaces

---
+++ dto/note.ts
@@ -0,0 +1,21 @@
+import { Note } from "../models/note";
+
+export interface NoteLink {
+  href: string;
+  method?: string;
+}
+
+export interface NoteResponse {
+  noteId: string;
+  noteName: string;
+  content: string;
+  tags?: string[];
+  isPinned: boolean;
+  pinnedPosition?: number;
+  isArchived: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  links: { [key: string]: NoteLink };
+}
+
+export interface NotePageResponse {}
