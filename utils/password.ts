1f9839be4cee50731eebebcca68fc71f497ef753
Dallas. S <andrewdallassherman@gmail.com>
2025-02-26 16:26:36 +0900

    password validation

---
+++ utils/password.ts
@@ -0,0 +1,22 @@
+export async function validatePassword(password: string): Promise<boolean> {
+  // Password must:
+  // - Be at least 8 characters long
+  // - Contain at least 2 numbers
+  // - Contain at least 2 special character
+
+  let hasNumber = false;
+  let hasSpecial = false;
+
+  if (password.length < 8) {
+    return false;
+  }
+  for (const char of password) {
+    if (/\d{2,}/.test(char)) {
+      hasNumber = true;
+    } else if (/(?:.*[^A-Za-z0-9].*){2,}/.test(char)) {
+      hasSpecial = true;
+    }
+  }
+
+  return hasNumber && hasSpecial;
+}
