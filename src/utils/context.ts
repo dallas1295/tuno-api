98ab3b1d4d6a373773e0f0986637d218f4da4d49
Dallas. S <andrewdallassherman@gmail.com>
2025-03-02 17:38:14 +0900

    oak migration

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,10 +1,12 @@
+import { Context } from "jsr:@oak/oak";
+
 export function createContext(timeout: number) {
   const controller = new AbortController();
   const signal = controller.signal;
   const timeoutId = setTimeout(() => controller.abort(), timeout);
 
   return {
-    ctx: { signal },
+    ctx: { signal } as Partial<Context>,
     cancel: () => clearTimeout(timeoutId),
   };
 }
