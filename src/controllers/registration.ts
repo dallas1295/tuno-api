0332d330a7c0b9ea77815d58bdcb62adf4e2c1fa
Dallas S. <andrewdallassherman@gmail.com>
2025-04-10 10:28:00 +0900

    tracking and errors

--- src/controllers/registration.ts
+++ src/controllers/registration.ts
@@ -1,6 +1,6 @@
 import { User } from "../models/user.ts";
 import { Response } from "../utils/response.ts";
-import { HTTPMetrics } from "../utils/metrics.ts";
+import { ErrorCounter, HTTPMetrics } from "../utils/metrics.ts";
 import { Context } from "@oak/oak";
 import { UserService } from "../services/user.ts";
 import { tokenService } from "../services/token.ts";
@@ -12,27 +12,42 @@ export async function registration(ctx: Context) {
     const body = await ctx.request.body.json();
 
     if (!body?.username || !body?.email || !body?.password) {
+      ErrorCounter.add(1, {
+        type: "validation",
+        operation: "register",
+      });
       return Response.badRequest(ctx, "Missing required fields");
     }
 
-    const userService = await UserService.initialize();
-    const user: User = await userService.createUser(
-      body.username,
-      body.email,
-      body.password,
-    );
+    try {
+      const userService = await UserService.initialize();
+      const user: User = await userService.createUser(
+        body.username,
+        body.email,
+        body.password,
+      );
 
-    const tokens = await tokenService.generateTokenPair(user);
+      const tokens = await tokenService.generateTokenPair(user);
 
-    return Response.success(ctx, {
-      message: "User registered successfully",
-      user: { username: user.username, email: user.email },
-      ...tokens,
-    });
+      return Response.success(ctx, {
+        message: "User registered successfully",
+        user: { username: user.username, email: user.email },
+        ...tokens,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return Response.badRequest(ctx, error.message);
+      }
+      return Response.badRequest(ctx, "Invalid registration request");
+    }
   } catch (error) {
+    ErrorCounter.add(1, {
+      type: "internal",
+      operation: "register,",
+    });
+    return Response.internalError(
+      ctx,
+      error instanceof Error ? error.message : "Error registering user",
+    );
   }
 }
-
-// TODO: need to fix the catch statements to use instance of to root out possible fail reasons... return unauthorized if it's DB error.
-// TODO: need to check if the set up for my success response in try block is accurate and what I want.
-// TODO: ensure that i make sure there's no other required responses in the try block
